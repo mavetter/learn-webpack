12d9c1e77bbec6d52550813554d23e61aea7c60b
mavetter <marielle.vetter@gmail.com>
2019-08-19 16:57:41 +0200

    css and postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
+// const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: ['./app.js', './app.css']
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/assets')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist/assets'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {importLoaders: 1},
+          },
+          {
+            loader: 'postcss-loader',
+          },
+        ],
+      },
+    ]
   }
 };
